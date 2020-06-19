8f035ea5640967e104f4f82cc1382827fc1f6122
Ilan Guenet <ilan.guenet@epita.fr>
2020-06-19 15:10:18 +0200

    [ADD][DECOMPRESSION] Algo for decompression done. Need to implement the save results for it.

--- ilan.guenet_LZW.py
+++ ilan.guenet_LZW.py
@@ -2,14 +2,13 @@ import argparse
 import pandas as pd
 
 
-def uncompress(file, dic):
-    """
-    Uncompress a file with an initial dictionary
-    :param file: file to decompress
-    :param dic: initial dictionary
-    :return: the string corresponding to the uncompression of the file
-    """
-    pass
+def size_in_bits(nb):
+    return nb.bit_length()
+
+
+def read_addr_n_bits(input, size):
+    res = input[:size]
+    return int(res, 2)
 
 
 def write_addr_n_bits(addr, size):
@@ -18,6 +17,61 @@ def write_addr_n_bits(addr, size):
     zeros = '0'*padding
     res = zeros + res
     return res
+
+
+def uncompress(file, dico, special_character='%'):
+    """
+    Uncompress a file with an initial dictionary
+    :param file: file to decompress
+    :param dico: initial dictionary
+    :return: the string corresponding to the uncompression of the file
+    """
+    max_addr = len(dico) - 1
+    addr_size = size_in_bits(max_addr)
+    output = ''
+    df = pd.DataFrame(columns=['Buffer', 'Input', 'New sequence', 'Address',
+                               'Output'])
+    buffer = ''
+    df_i = 0
+    while file:
+        addr = read_addr_n_bits(file, addr_size)
+        file = file[addr_size:]
+        input = dico[addr]
+        if special_character in input:
+            # increase the size by the number of special character
+            addr_size += len(input)
+            input_print = f"@D[{input}]={addr}"
+            df.loc[df_i] = [buffer, input_print, '', '', '']
+            df_i += 1
+        else:
+            new_seq = buffer + input[0]
+
+            if new_seq in dico:
+                input_print = f"@D[{input}]={addr}"
+                df.loc[df_i] = [buffer, input_print, '', '', '']
+                df_i += 1
+                buffer += new_seq
+            else:
+                dico.append(new_seq)
+                max_addr += 1
+
+                buffer = new_seq[-1:] + input[1:]
+                output_local = new_seq[:-1]
+
+                input_print = f"@D[{input}]={addr}"
+                df.loc[df_i] = [buffer, input_print, new_seq, max_addr,
+                                output_local]
+                df_i += 1
+                output += output_local
+
+    # No more character available. Hence empty the buffer
+    df.loc[df_i] = [buffer, '', '', '', buffer]
+    df_i += 1
+    output += buffer
+
+    print(df)
+    print(output)
+    return output
 
 
 def make_dico(file, special_character='%'):
@@ -158,8 +212,6 @@ def get_file_content(filename):
     return file_content
 
 
-def size_in_bits(nb):
-    return nb.bit_length()
 
 
 if __name__ == '__main__':
